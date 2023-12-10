55a0f7a90e206f5304cc5ca24842dc7f602612e2
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-10 14:33:41 +0900

    Update 1_Usage.py

--- pages/1_Usage.py
+++ pages/1_Usage.py
@@ -13,9 +13,11 @@ st.subheader('Input Music')
 st.write('Input music to be used in the created game. Choose input method from YoutubeDL, Spotify API, Audiostock, Uploader. YoutubeDL supports https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md.')
 
 l, r = st.columns(2, gap='medium')
+
 with l:
     st.subheader('Scene of Input Music')
     st.write('Input the scene of input music. Valence and Arousal are based on https://en.wikipedia.org/wiki/Emotion_classification#Circumplex_model.')
+
 with r:
     st.subheader('Scene of Output Music')
     st.write('Input the scene of output music. Valence and Arousal are based on https://en.wikipedia.org/wiki/Emotion_classification#Circumplex_model.')
