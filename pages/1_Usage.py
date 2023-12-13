f228c3337f6685be1ba50c0d4055b44c306a1d22
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-13 18:30:28 +0900

    Update 1_Usage.py

--- pages/1_Usage.py
+++ pages/1_Usage.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 st.set_page_config(page_title='Test App', page_icon=':musical_note:', layout='wide')
 
+st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_container_width=True)
+if st.sidebar.button('Clear Cache', use_container_width=True):
+   st.cache_data.clear()
+
 st.title('Usage')
 
 st.header('Video on Usage of Test App')
