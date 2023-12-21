6974a8364b77e9476e797d749384db39127aca88
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-22 01:16:21 +0900

    Update 3_Library.py

--- pages/3_Library.py
+++ pages/3_Library.py
@@ -4,14 +4,6 @@ def pdf(i, w, h):
     s = f'https://drive.google.com/file/d/{i}/preview'
     st.markdown(f'<iframe src="{s}" width="{w}" height="{h}"></iframe>', True)
 
-# Google Driveの共有リンク
-id = '18H8dnbhL-D53BUWb09nGVWGWyot06OM6'
-google_drive_link = f'https://drive.google.com/file/d/{id}/preview'
-
-# iframeで埋め込む
-iframe = f'<iframe src="{google_drive_link}" width="600" height="400"></iframe>'
-st.markdown(iframe, True)
-
 st.header('Paper')
 pdf('18H8dnbhL-D53BUWb09nGVWGWyot06OM6', 600, 900)
 
