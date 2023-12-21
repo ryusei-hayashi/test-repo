637c66872551d9ffeabff8dbbc4764dc3e297779
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-21 20:37:27 +0900

    Update 3_Library.py

--- pages/3_Library.py
+++ pages/3_Library.py
@@ -1,17 +1,26 @@
 import streamlit as st
+import base64
+import os
+
+if not os.path.exists('docs'):
+    download_folder(id='1oSzl_7l9SuJ32PuG3qDTsbzwCPErgxuA')
 
 st.set_page_config('EgGMAn', ':egg:', 'wide')
 st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_container_width=True)
 if st.sidebar.button('Clear Cache', use_container_width=True):
    st.cache_data.clear()
 
+def pdf(f, w, h):
+    s = f'data:application/pdf;base64,{base64.b64encode(open(f, "rb").read()).decode("utf-8")}'
+    st.markdown(f'<iframe src="{s}" width="{w}" height="{h}" type="application/pdf"></iframe>', True)
+
 st.title('Library')
 
 st.header('Paper')
-st.write('https://drive.google.com/file/d/18H8dnbhL-D53BUWb09nGVWGWyot06OM6')
+pdf('docs/paper.pdf', 500, 800)
 
 st.header('Slide')
-st.write('https://drive.google.com/file/d/1MIN0am-lZV7TqGx20CjgG1CSQXyLkgOO')
+pdf('docs/slide.pdf', 500, 400)
 
 st.header('Panel')
-st.write('https://drive.google.com/file/d/1BY0bn5FWc_d2yoEi4ssPpgdomMCw9fLQ')
+pdf('docs/panel.pdf', 500, 800)
