20a24fc92f6c20a48665aad152243eddce14236b
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-22 01:15:24 +0900

    Update 3_Library.py

--- pages/3_Library.py
+++ pages/3_Library.py
@@ -12,4 +12,11 @@ google_drive_link = f'https://drive.google.com/file/d/{id}/preview'
 iframe = f'<iframe src="{google_drive_link}" width="600" height="400"></iframe>'
 st.markdown(iframe, True)
 
+st.header('Paper')
+pdf('18H8dnbhL-D53BUWb09nGVWGWyot06OM6', 600, 900)
+
+st.header('Slide')
+pdf('1MIN0am-lZV7TqGx20CjgG1CSQXyLkgOO', 600, 400)
+
+st.header('Panel')
 pdf('1BY0bn5FWc_d2yoEi4ssPpgdomMCw9fLQ', 600, 900)
