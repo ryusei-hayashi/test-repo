1ee13fe188a88a447aaad40edd4ec37b87149443
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-21 17:31:54 +0900

    Update 1_Usage.py

--- pages/1_Usage.py
+++ pages/1_Usage.py
@@ -14,14 +14,14 @@ st.write('https://drive.google.com/file/d/1ICV-xUKHAVmSvUUTvPABV1KQIFMyXXij') #s
 st.header('Text')
 
 st.subheader('Input Music')
-st.write('Input music to be used in the created game. Choose input way from Spotify API, Audiostock, YoutubeDL, Uploader. YoutubeDL supports https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md.')
+st.markdown('Input music to be used in the created game. Choose input way from Spotify API, Audiostock, YoutubeDL, Uploader. YoutubeDL has many [Supported sites](https://github.com/yt-dlp/yt-dlp/blob/master/supportedsites.md)')
 
 l, r = st.columns(2, gap='medium')
 
 with l:
     st.subheader('Scene of Input Music')
-    st.write('Input the scene of input music. Valence and Arousal are based on https://en.wikipedia.org/wiki/Emotion_classification#Circumplex_model.')
+    st.markdown('Input the scene of input music. Valence and Arousal are based on [Circumplex Model](https://en.wikipedia.org/wiki/Emotion_classification#Circumplex_model).')
 
 with r:
     st.subheader('Scene of Output Music')
-    st.write('Input the scene of output music. Valence and Arousal are based on https://en.wikipedia.org/wiki/Emotion_classification#Circumplex_model.')
+    st.markdown('Input the scene of output music. Valence and Arousal are based on [Circumplex Model](https://en.wikipedia.org/wiki/Emotion_classification#Circumplex_model).')
