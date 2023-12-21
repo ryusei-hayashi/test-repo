5bd5e69621e34d099312d3476a0c46ad1a204dd8
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-22 01:17:48 +0900

    Update 3_Library.py

--- pages/3_Library.py
+++ pages/3_Library.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.set_page_config('EgGMAn', ':egg:', 'wide')
+st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_container_width=True)
+if st.sidebar.button('Clear Cache', use_container_width=True):
+    st.cache_data.clear()
+
 def pdf(i, w, h):
     s = f'https://drive.google.com/file/d/{i}/preview'
     st.markdown(f'<iframe src="{s}" width="{w}" height="{h}"></iframe>', True)
