67127f39019a48a01a1e24efee90b54b5fcf0d75
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-21 17:38:01 +0900

    Update 4_Profile.py

--- pages/4_Profile.py
+++ pages/4_Profile.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title='Test App', page_icon=':musical_note:', layout='wide')
-
+st.set_page_config('EgGMAn', ':egg:', 'wide')
 st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_container_width=True)
 if st.sidebar.button('Clear Cache', use_container_width=True):
    st.cache_data.clear()
