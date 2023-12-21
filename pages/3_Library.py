b625011f2673055e0013b8ba59657940dc134518
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-22 01:06:00 +0900

    Update 3_Library.py

--- pages/3_Library.py
+++ pages/3_Library.py
@@ -1,21 +1,8 @@
 import streamlit as st
 
-st.set_page_config('EgGMAn', ':egg:', 'wide')
-st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_container_width=True)
-if st.sidebar.button('Clear Cache', use_container_width=True):
-    st.cache_data.clear()
+# Google Driveの共有リンク
+google_drive_link = "https://drive.google.com/file/d/1CElf11tsDnjplpgmkTtW5yUhcXQi-yU5/preview"
 
-def pdf(i, w, h):
-    s = f'https://drive.google.com/file/d/{i}/preview'
-    st.markdown(f'<iframe src="{s}" width="{w}" height="{h}"></iframe>', True)
-
-st.title('Library')
-
-st.header('Paper')
-pdf('18H8dnbhL-D53BUWb09nGVWGWyot06OM6', 600, 900)
-
-st.header('Slide')
-pdf('1MIN0am-lZV7TqGx20CjgG1CSQXyLkgOO', 600, 400)
-
-st.header('Panel')
-pdf('1BY0bn5FWc_d2yoEi4ssPpgdomMCw9fLQ', 600, 900)
+# iframeで埋め込む
+iframe = f'<iframe src="{google_drive_link}" width="600" height="400"></iframe>'
+st.markdown(iframe, unsafe_allow_html=True)
