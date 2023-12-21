706e26dac24fecfa6129026bfd50db85a0e8e58a
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-22 01:09:00 +0900

    Update 3_Library.py

--- pages/3_Library.py
+++ pages/3_Library.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 # Google Driveの共有リンク
-google_drive_link = "https://drive.google.com/file/d/1CElf11tsDnjplpgmkTtW5yUhcXQi-yU5/preview"
+id = '1CElf11tsDnjplpgmkTtW5yUhcXQi-yU5'
+google_drive_link = f'https://drive.google.com/file/d/{id}/preview'
 
 # iframeで埋め込む
 iframe = f'<iframe src="{google_drive_link}" width="600" height="400"></iframe>'
