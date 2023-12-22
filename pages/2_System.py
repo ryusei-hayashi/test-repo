88665b4cc18de9443c5f6008c7c481686ce96ac6
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-22 22:13:10 +0900

    Update 2_System.py

--- pages/2_System.py
+++ pages/2_System.py
@@ -13,17 +13,17 @@ st.write('comming soon') #st.video()
 st.header('Text')
 
 st.subheader('Input Music')
-st.write('Convert the input music to the coordinate z in VAE.')
+st.markdown('- Convert the input music to the coordinate z in VAE')
 
 l, r = st.columns(2, gap='medium')
 
 with l:
     st.subheader('Scene of Input Music')
-    st.write('Compute the center p of the scene of input music.')
+    st.markdown('- Compute the center p of the scene of input music')
 
 with r:
     st.subheader('Scene of Output Music')
-    st.write('Compute the center q of the scene of output music.')
+    st.markdown('- Compute the center q of the scene of output music')
 
 st.subheader('Output Music')
-st.write('Move the coordinate z in the vector q - p direction and retrieve music near the moved coordinate z.')
+st.markdown('- Move the coordinate z in the vector q - p direction\n- Retrieve music near the moved coordinate z')
