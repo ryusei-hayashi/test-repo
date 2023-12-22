52806abb71ee999c15ea3564d6b17a7811e740df
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-22 22:14:32 +0900

    Update 4_Profile.py

--- pages/4_Profile.py
+++ pages/4_Profile.py
@@ -3,11 +3,11 @@ import streamlit as st
 st.set_page_config('EgGMAn', ':egg:', 'wide')
 st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_container_width=True)
 if st.sidebar.button('Clear Cache', use_container_width=True):
-   st.cache_data.clear()
+    st.cache_data.clear()
 
 def profile(n, a, p, e):
-   st.subheader(n)
-   st.markdown(f'- Affiliation: {a}\n- Position: {p}\n- E-Mail: {e}')
+    st.subheader(n)
+    st.markdown(f'- Affiliation: {a}\n- Position: {p}\n- E-Mail: {e}')
 
 st.title('Profile')
 
