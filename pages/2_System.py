a50c00bd939ec7f0732d3a4735d20a255239845d
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-22 22:51:38 +0900

    Update 2_System.py

--- pages/2_System.py
+++ pages/2_System.py
@@ -23,11 +23,11 @@ l, r = st.columns(2, gap='medium')
 
 with l:
     st.subheader('Scene of Input Music')
-    st.markdown('- Compute the center p of the scene of input music')
+    st.markdown('- Collect music for the same scene as the input scene\n- Convert collected music to coordinates\n- Compute the center p of the coordinates')
 
 with r:
     st.subheader('Scene of Output Music')
-    st.markdown('- Compute the center q of the scene of output music')
+    st.markdown('- Collect music for the same scene as the input scene\n- Convert collected music to coordinates\n- Compute the center q of the coordinates')
 
 st.subheader('Output Music')
 st.markdown('- Move the coordinate z in the vector q - p direction\n- Retrieve music near the moved coordinate z')
