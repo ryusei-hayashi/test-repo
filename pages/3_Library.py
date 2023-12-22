9d3fc7d3b47f46c004f1e95d1354282156c553ab
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-22 22:04:51 +0900

    Update 3_Library.py

--- pages/3_Library.py
+++ pages/3_Library.py
@@ -5,17 +5,17 @@ st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_
 if st.sidebar.button('Clear Cache', use_container_width=True):
     st.cache_data.clear()
 
-def preview(i, w, h):
+def view(i, w, h):
     s = f'https://drive.google.com/file/d/{i}/preview'
     st.markdown(f'<iframe src="{s}" width="{w}" height="{h}"></iframe>', True)
 
 st.title('Library')
 
 st.header('Paper')
-preview('18H8dnbhL-D53BUWb09nGVWGWyot06OM6', 700, 990)
+view('18H8dnbhL-D53BUWb09nGVWGWyot06OM6', 700, 990)
 
 st.header('Slide')
-preview('1MIN0am-lZV7TqGx20CjgG1CSQXyLkgOO', 700, 420)
+view('1MIN0am-lZV7TqGx20CjgG1CSQXyLkgOO', 700, 420)
 
 st.header('Panel')
-preview('1BY0bn5FWc_d2yoEi4ssPpgdomMCw9fLQ', 700, 990)
+view('1BY0bn5FWc_d2yoEi4ssPpgdomMCw9fLQ', 700, 990)
