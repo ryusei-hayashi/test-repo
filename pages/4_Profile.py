78014b62b18b02703f437e9436956833df1534b7
ryusei <97586316+ryusei-hayashi@users.noreply.github.com>
2023-12-30 20:01:03 +0900

    Update 4_Profile.py

--- pages/4_Profile.py
+++ pages/4_Profile.py
@@ -2,8 +2,6 @@ import streamlit as st
 
 st.set_page_config('Test App', ':test_tube:', 'wide')
 st.sidebar.link_button('Contact Us', 'https://forms.gle/A4vWuEAp4pPEY4sf9', use_container_width=True)
-if st.sidebar.button('Clear Cache', use_container_width=True):
-    st.cache_data.clear()
 
 def profile(n, a, p, e):
     st.subheader(n)
